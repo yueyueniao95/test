19bdbf1b65c000c41288f3d26c82edcdea9c2ab9
dapengsun <dapeng.sun@aiyuangong.com>
2020-06-15 16:57:09 +0800

    111

---
+++ src/main/java/com/github/test/App.java
@@ -0,0 +1,13 @@
+package com.github.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
